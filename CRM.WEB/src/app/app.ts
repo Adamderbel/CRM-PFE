5242f69551d477007be55493185e86e00a0b38af
mannaiahmedgit <mannai.ahmed5555@gmail.com>
2026-02-10 23:08:11 +0100

    add composant

--- CRM.WEB/src/app/app.ts
+++ CRM.WEB/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./layout/navbar/navbar";
+import { Layout } from "./layout/layout/layout";
+import { Sidebar } from "./layout/sidebar/sidebar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Layout, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- CRM.WEB/src/app/layout/layout/layout.ts
+++ CRM.WEB/src/app/layout/layout/layout.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { Navbar } from "../navbar/navbar";
+import { Sidebar } from "../sidebar/sidebar";
+
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [Navbar, Sidebar],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
