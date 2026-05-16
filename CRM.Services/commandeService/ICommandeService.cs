813dc6c6ac034c3ad445f57a2841591afe9c31db
mannaiahmedgit <mannai.ahmed5555@gmail.com>
2026-05-17 01:07:55 +0200

    add serivice cmd

---
+++ CRM.Services/commandeService/ICommandeService.cs
@@ -0,0 +1,19 @@
+﻿using CRM.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Services.commandeService
+{
+    public interface ICommandeService
+    {
+        Task<List<CermCommande>> GetCommandesByClientAsync(
+     string clientId);
+
+        Task<CermCommande?> GetCommandeAsync(
+            string refCommande);
+        Task<List<CermCommandeLigne>> GetLignesCommandeByRefAsync(string refCommande);
+    }
+}
